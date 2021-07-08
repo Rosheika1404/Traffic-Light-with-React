88df04c5411209715cc1b2d4e3dacd8f63ea1d7a
Sheika1404 <rosheika1404@gmail.com>
2021-07-08 19:01:19 +0000

    add glow to red

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [selectColor, setSelectColor] = useState("");
+
 	return (
 		<div>
 			<div className="pole"></div>
 			<div className="text-center trafficLight">
-				<div className="light red"></div>
+				<div
+					className={
+						"light red " + (selectColor === "red" ? "selected" : "")
+						// if (selectColor === "red"){
+						// 	setSelectColor{"selected"};
+						// }else{
+						// 	""
+					}
+					onClick={() => setSelectColor("red")}></div>
 				<div className="light yellow"></div>
 				<div className="light green"></div>
 			</div>
